bb2de9029c88dd9977f85f6195e50b59b764d2bd
yueguchi <y.eguchi0526@gmail.com>
2016-06-26 01:07:44 +0900

    barcode連携

--- Assets/scripts/util/UnityAdd.cs
+++ Assets/scripts/util/UnityAdd.cs
@@ -11,12 +11,64 @@ public class UnityAdd : MonoBehaviour {
 	}
 	
 	public void ShowAd() {
-		// 広告再生の準備ができているか確認。
-		if (Advertisement.IsReady ()) {
-			// 準備ができていたら、広告再生。
-			Advertisement.Show ();
-			// birthSceneに遷移
-			new SceneManager ().showBirthScene ();
+		using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+			using (AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) {
+				activity.Call("setListener", new ActivityListener());
+				activity.Call ("readQRCode", activity);
+			}
+		}
+
+//		// 広告再生の準備ができているか確認。
+//		if (Advertisement.IsReady ()) {
+//			// 準備ができていたら、広告再生。
+//			Advertisement.Show ();
+//			// birthSceneに遷移
+//			new SceneManager ().showBirthScene ();
+//		}
+	}
+
+	public class ActivityListener : AndroidJavaProxy
+	{
+		public ActivityListener()
+			: base("palcom.fantasyfarmunitylibrary.QRCodeListener")
+		{
+		}
+
+		public void onRestart()
+		{
+		}
+
+		public void onStart()
+		{
+		}
+
+		public void onResume()
+		{
+		}
+
+		public void onPause()
+		{
+		}
+
+		public void onStop()
+		{
+		}
+
+		/**
+		 * dataにバーコードで読み取った結果テキストが返る
+		 */
+		public void onActivityResult(int requestCode, int resultCode, string data)
+		{
+			AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject activity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+			activity.Call ("runOnUiThread", new AndroidJavaRunnable (() => {
+				AndroidJavaObject alertDialogBuilder = new AndroidJavaObject ("android.app.AlertDialog$Builder", activity);
+				alertDialogBuilder.Call<AndroidJavaObject> ("setMessage", data);
+				alertDialogBuilder.Call<AndroidJavaObject> ("setCancelable", true);
+				alertDialogBuilder.Call<AndroidJavaObject> ("setPositiveButton", "OK", null);
+				AndroidJavaObject dialog = alertDialogBuilder.Call<AndroidJavaObject> ("create");
+				dialog.Call ("show");
+			}));
 		}
 	}
 }
